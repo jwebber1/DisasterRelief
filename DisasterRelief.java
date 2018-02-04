b6e9a6f54d6caf695446f2289994768162dc18d3
jwebber1 <34067696+jwebber1@users.noreply.github.com>
2018-02-04 17:51:10 -0600

    Update .02
    Accessing the database and able to make successful, working queries. Accepts zipcodes (will not accept zipcodes that have  letters or are too large or small). Currently connected to zips2 and looking at its zipcodes.

--- DisasterRelief.java
+++ DisasterRelief.java
@@ -8,10 +8,27 @@ public class DET_02_DisasterRelief {
     static Statement statement;
     public static void main(String[] args) {
         //create scanner for user input of zip code and miles
-        /*
         Scanner scan = new Scanner(System.in);
         System.out.print("Please enter 5-digit zip code: ");
         String zipCode = scan.next();
+
+        //loop to ensure zipCode is correct
+        while (!zipCode.matches("[0-9]+") || zipCode.length() != 5){
+            if(!zipCode.matches("[0-9]+")) {
+                System.out.print("An alphabetical letter was used. Please enter 5-digit zip code: ");
+                zipCode = scan.next();
+            }
+            else if (zipCode.length()<5){
+                System.out.print("Too few digits used. Please enter 5-digit zip code: ");
+                zipCode = scan.next();
+            }
+            else{
+                System.out.print("Too many digits used. Please enter 5-digit zip code: ");
+                zipCode = scan.next();
+            }
+        }
+
+        /*
         System.out.print("Please enter miles from disaster wanted: ");
         double distInMiles = scan.nextDouble();
         double distInKilos = distInMiles * 1.60934;
@@ -21,78 +38,99 @@ public class DET_02_DisasterRelief {
         //credentials to use to access the database
         String host = "jdbc:mysql://turing.cs.missouriwestern.edu:3306/misc";
         String user = "csc254";
-        String password="age126";
+        String password= "age126";
 
         //string to be fed into the database query
-        String queryString = "SELECT city, region, country, latitude, longitude FROM cities where region LIKE 'MO' LIMIT 25";
-
+        String queryString1 = "SELECT city, state, locationtype, zipcode, zipcodetype, lat, `long` FROM zips2 where zipcode = "+zipCode+" AND locationtype = 'PRIMARY' LIMIT 25";
         try {
             //create a connection to the database
             connection = DriverManager.getConnection(host, user, password);
 
-            //create a statement, set it to be the above queryString
+            //create a statement, set it to be the above queryString1
             statement = connection.createStatement();
-            rsSet = statement.executeQuery(queryString);
-
-
-
+            rsSet = statement.executeQuery(queryString1);
 
             while(rsSet.next()){
-                String country = rsSet.getString("country");
-                String name = rsSet.getString("city");
-                String region = rsSet.getString("region");
-                double lat = rsSet.getDouble("latitude");
-                double lon = rsSet.getDouble("longitude");
-                Place place = new Place(name,region,country,lat,lon);
-                System.out.println(place);
+                //System.out.println("k");
+
+                String city = rsSet.getString("city");
+                String state = rsSet.getString("state");
+                String locationtype = rsSet.getString("locationtype");
+                String zipcode = rsSet.getString("zipcode");
+                String zipcodetype = rsSet.getString("zipcodetype");
+                double lat = rsSet.getDouble("lat");
+                double lon = rsSet.getDouble("long");
+                Place newPlace = new Place(city,state,locationtype,zipcode,zipcodetype,lat,lon);
+                System.out.println(newPlace);
+
             }
 
             connection.close();
         } catch (SQLException e) {
-            System.out.println("Trouble connecting to the database");
+            System.out.println("Trouble accessing information in the database");
             System.exit(1);
         }
     }
 }
 
 class Place {
-    String name;
-    String region;
-    String country;
+
+    String city;
+    String state;
+    String locationType;
+    String zipCode;
+    String zipCodeType;
     double latitude;
     double longitude;
-    double distFromOrigin;
 
-    public Place(String name, String region, String country, double latitude, double longitude) {
-        this.name = name;
-        this.region = region;
-        this.country = country;
+    public Place(String city, String state, String locationType, String zipCode, String zipCodeType, double latitude, double longitude) {
+        this.city = city;
+        this.state = state;
+        this.locationType = locationType;
+        this.zipCode = zipCode;
+        this.zipCodeType = zipCodeType;
         this.latitude = latitude;
         this.longitude = longitude;
     }
 
-    public String getName() {
-        return name;
+    public String getCity() {
+        return city;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setCity(String city) {
+        this.city = city;
     }
 
-    public String getRegion() {
-        return region;
+    public String getState() {
+        return state;
     }
 
-    public void setRegion(String region) {
-        this.region = region;
+    public void setState(String state) {
+        this.state = state;
     }
 
-    public String getCountry() {
-        return country;
+    public String getLocationType() {
+        return locationType;
     }
 
-    public void setCountry(String country) {
-        this.country = country;
+    public void setLocationType(String locationType) {
+        this.locationType = locationType;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getZipCodeType() {
+        return zipCodeType;
+    }
+
+    public void setZipCodeType(String zipCodeType) {
+        this.zipCodeType = zipCodeType;
     }
 
     public double getLatitude() {
@@ -111,23 +149,16 @@ class Place {
         this.longitude = longitude;
     }
 
-    public double getDistFromOrigin() {
-        return distFromOrigin;
-    }
-
-    public void setDistFromOrigin(double distFromOrigin) {
-        this.distFromOrigin = distFromOrigin;
-    }
-
     @Override
     public String toString() {
         return "Place{" +
-                "name='" + name + '\'' +
-                ", region='" + region + '\'' +
-                ", country='" + country + '\'' +
+                "city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", locationType='" + locationType + '\'' +
+                ", zipCode='" + zipCode + '\'' +
+                ", zipCodeType='" + zipCodeType + '\'' +
                 ", latitude=" + latitude +
                 ", longitude=" + longitude +
-                ", distFromOrigin=" + distFromOrigin +
                 '}';
     }
 }
