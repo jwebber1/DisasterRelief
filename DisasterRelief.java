5125359307b35d2b707cab663ecb84fceaf64523
jwebber1 <34067696+jwebber1@users.noreply.github.com>
2018-02-04 21:06:03 -0600

    Updated .04
    Currently gets a zipcode and miles from origin, and will pump out (to the console) all cities within that distance as well as their distance from the origin. Does not repeat cities that have multiple zipcodes.

--- DisasterRelief.java
+++ DisasterRelief.java
@@ -9,7 +9,7 @@ public class DET_02_DisasterRelief {
     public static void main(String[] args) {
         //create scanner for user input of zip code and miles
         Scanner scan = new Scanner(System.in);
-        
+
         System.out.print("Please enter 5-digit zip code: ");
         String zipCode = scan.next();
 
@@ -32,42 +32,51 @@ public class DET_02_DisasterRelief {
         System.out.print("Please enter miles from disaster wanted: ");
         double distInMiles = scan.nextDouble();
 
-
+        //loop to ensure distance isn't negative
+        while(distInMiles <= 0){
+            System.out.println("It appears a negative number was entered. Please use positive numbers for distance.");
+            System.out.print("Please enter miles from disaster wanted: ");
+            distInMiles = scan.nextDouble();
+        }
 
         //credentials to use to access the database
         String host = "jdbc:mysql://turing.cs.missouriwestern.edu:3306/misc";
         String user = "csc254";
         String password= "age126";
 
-        //string to be fed into the database query
+        //strings to be fed into the database queries
         String queryStringAll = "SELECT city, state, locationtype, zipcode, taxreturnsfiled, lat, `long` FROM zips2 where locationtype = 'PRIMARY'";
         String queryStringForZip = "SELECT zipcode, lat, `long` FROM zips2 where locationtype = 'PRIMARY' AND zipcode = "+zipCode;
-
+        String queryStringHousing = "SELECT housingunits FROM zips";
 
         try {
             //create a connection to the database
             connection = DriverManager.getConnection(host, user, password);
 
-            double startLat = 0;
-            double startLong = 0;
+            //temporary variables to start latitude and longitude
+            double startLat = 0.0;
+            double startLong = 0.0;
 
+            //query to find latitude and longitude for initial zipCode
             statement = connection.createStatement();
             rsSet = statement.executeQuery(queryStringForZip);
-
             while(rsSet.next()) {
                 startLat = rsSet.getDouble("lat");
                 startLong = rsSet.getDouble("long");
             }
-            //System.out.println(startLat);
-            //System.out.println(startLong);
+
+            //create arraylist to hold cities within specified range and another to help get the amount of housing units
+            ArrayList<Place> disasterCities = new ArrayList<>();
+            ArrayList<Place> housingArray = new ArrayList<>();
 
             //create a statement, set it to be the above queryStringAll
             statement = connection.createStatement();
             rsSet = statement.executeQuery(queryStringAll);
 
-            while(rsSet.next()){
-                //System.out.println("k");
+            int count = 0;
 
+            //looking through zips2
+            while(rsSet.next()){
                 String city = rsSet.getString("city");
                 String state = rsSet.getString("state");
                 String locationtype = rsSet.getString("locationtype");
@@ -75,15 +84,32 @@ public class DET_02_DisasterRelief {
                 int taxreturnsfiled = rsSet.getInt("taxreturnsfiled");
                 double lat = rsSet.getDouble("lat");
                 double lon = rsSet.getDouble("long");
-                Place newPlace = new Place(city,state,locationtype,zipcode,taxreturnsfiled,lat,lon);
-                if(haversine.totDistance(startLat, startLong, lat, lon)<distInMiles) {
-                    System.out.println(newPlace + "\td= " + haversine.totDistance(startLat, startLong, lat, lon));
+                Place place = new Place(city,state,locationtype,zipcode,taxreturnsfiled,lat,lon);
+                place.setDistFromOrigin(haversine.totDistance(startLat, startLong, lat, lon));
+                if(place.getDistFromOrigin()<distInMiles) {
+                    disasterCities.add(place);
+                    //System.out.println(place + "\td= " + haversine.totDistance(startLat, startLong, lat, lon));
                 }
-
-
+            }
+            
+            //gets rid of repeat cities in the arraylist if the one to the right has the same City AND State
+            for(int i = 0; i<disasterCities.size()-1; i++){
+                if(disasterCities.get(i).getCity().equals(disasterCities.get(i+1).getCity()) && disasterCities.get(i).getState().equals(disasterCities.get(i+1).getState())){
+                    disasterCities.remove(i+1);
+                    i--;
+                }
             }
 
-            //System.out.println(haversine.totDistance(40, -90, 41, -74));
+            //create a statement, set it to be the above queryStringHousing
+            statement = connection.createStatement();
+            rsSet = statement.executeQuery(queryStringHousing);
+            
+            //Currently prints out the array to the console
+            for(int i = 0; i<disasterCities.size(); i++){
+                System.out.println("Place: "+disasterCities.get(i).getCity()+", "+disasterCities.get(i).getState()+"\t"+
+                    disasterCities.get(i).zipCode+",\t distance = "+disasterCities.get(i).getDistFromOrigin());
+            }
+
             connection.close();
         } catch (SQLException e) {
             System.out.println("Trouble accessing information in the database");
@@ -101,6 +127,7 @@ class Place {
     int taxreturnsfiled;
     double latitude;
     double longitude;
+    double distFromOrigin;
 
     public Place(String city, String state, String locationType, String zipCode, int taxreturnsfiled, double latitude, double longitude) {
         this.city = city;
@@ -110,6 +137,7 @@ class Place {
         this.taxreturnsfiled = taxreturnsfiled;
         this.latitude = latitude;
         this.longitude = longitude;
+        this.distFromOrigin = 0.0;
     }
 
     public String getCity() {
@@ -168,6 +196,13 @@ class Place {
         this.longitude = longitude;
     }
 
+    public double getDistFromOrigin() {
+        return distFromOrigin;
+    }
+
+    public void setDistFromOrigin(double distFromOrigin) {
+        this.distFromOrigin = distFromOrigin;
+    }
     @Override
     public String toString() {
         return "Place{" +
